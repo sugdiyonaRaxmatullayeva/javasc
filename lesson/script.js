27cd438a5d8c76d594178ab909703bf5772ba7e5
soulnaya <sugdiyona711@gmail.com>
2023-02-03 06:03:26 -0800

    commit 1

---
+++ lesson/script.js
@@ -0,0 +1,40 @@
+"use script"
+alert(" lesson ")
+//DATE TYPE
+let name1 ="ism" ;
+
+console.log(name1);
+
+//underfined
+
+let text
+
+console.log(text);
+
+
+let title = "mening ismim";
+let namee ="gulsevar";
+let  ago = "18" ;
+let data = `${title} ${namee} va men ${ago} yoshdaman`;
+
+console.log(data);
+
+
+//lesson 
+"use script"
+let yil = "2023" ;
+let num = prompt ("how old are you")
+
+console.log( yil-num);
+
+//
+
+let a  = Number(prompt("a"));
+let b =Number(prompt("b"));
+let c= Number(prompt("c"));
+let d = Number(prompt("d"));
+
+
+console.log(((a+b)/d)*c);
+
+console.log(((a+b)**3)/((d+c)**2));
