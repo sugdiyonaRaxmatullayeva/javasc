d3342cde4d6977913e77f623368065d5d1723075
soulnaya <sugdiyona711@gmail.com>
2023-02-13 06:14:21 -0800

    commit 3

---
+++ lesson/lesson7/script.js
@@ -0,0 +1,37 @@
+"use script"
+// let arr=[
+//     [1,2,3,4,5],
+//     ["sevara","hadicha",[1,40,70,90],"shahzoda"],
+//     ["html","css","sass"],
+// ];
+// console.log(arr[1][2][3]);
+
+// const use1={
+//     fName:"Gulsevar",
+//     login:"Gulsevar",
+//     pasword:"dfogkc2543677",
+//     email:"sugdiyona@gmail.com",
+//     phoneNumber: "998 789 12 23",
+//     year:2005,
+//     now:2023,
+
+//     place:{
+//         country:"uzbekistan",
+//         city:"tashkent",
+//     },
+//     howold:function () {
+//       let a=this.now - this.year
+//     },
+
+// };
+
+
+for (let i = 0; i <=10; i++) {
+   console.log(isNaN);
+    
+}
+let arr=[1,2,3,4,5,6];
+for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+    
+}
